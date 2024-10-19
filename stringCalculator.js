2b402eabf97c28e6c55e5eda95b6de958b8fc461
sangamSanjeet <factsbe0@gmail.com>
2024-10-19 09:24:01 +0530

    [STEP-1] : basic add with edge cases

---
+++ stringCalculator.js
@@ -0,0 +1,6 @@
+// stringCalculator.js
+function add(numbers) {
+	return numbers === "" ? 0 : null;
+}
+
+module.exports = add;
---
+++ stringCalculator.test.js
@@ -0,0 +1,5 @@
+const add = require("./stringCalculator.js");
+
+test("should return 0 for an empty string", () => {
+	expect(add("")).toBe(0);
+});
