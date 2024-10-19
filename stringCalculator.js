78b21150ab80edf5354949bda05981e6046e0ebe
sangamSanjeet <factsbe0@gmail.com>
2024-10-19 09:25:05 +0530

    [STEP-1] : basic add with edge cases, updated

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,6 +1,18 @@
-// stringCalculator.js
-function add(numbers) {
-	return numbers === "" ? 0 : null;
+function add(numbersString) {
+	if (!numbersString) return 0;
+
+	const numbersArray = numbersString.split(",").map(Number);
+	let sum = 0;
+
+	for (const number of numbersArray) {
+		if (!number) {
+			sum += 0;
+		} else {
+			sum += number;
+		}
+	}
+
+	return sum;
 }
 
 module.exports = add;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -2,4 +2,8 @@ const add = require("./stringCalculator.js");
 
 test("should return 0 for an empty string", () => {
 	expect(add("")).toBe(0);
+	expect(add("1")).toBe(1);
+	expect(add("1,5")).toBe(6);
+	expect(add("1,5,1,0")).toBe(7);
+	expect(add(`7,""`)).toBe(7);
 });
